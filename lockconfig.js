ca7782591f45c80c1f7c0d5f32ac964d92be9b80
scasperski-git <scasperski@gmail.com>
2026-06-07 13:25:09 +0800

    Update lockconfig.js

--- lockconfig.js
+++ lockconfig.js
@@ -1,2 +1,2 @@
 const API_KEY = "SCZP_SECURE_2026";
-const API_URL="https://script.google.com/macros/s/AKfycbyK7SVh1VbF2pyycsI24Up8oR5xnWoeRGBh-IXAt-wjrlkLxktSiZGJM_4Bab-8SNlo/exec";
+const API_URL="https://script.google.com/macros/s/AKfycbxLNNsSxvMrT-z3_hIP_yZlosDjK_sUzx0iGpfWJhwTan35ENkc1DzEbVNuhhecacDM/exec";
